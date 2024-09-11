8a4fa6eaddd83cf6fceba7dca3c768c59b91d50a
Darajaw <darajaw@gmail.com>
2024-09-11 12:25:18 -0400

    Improved top command function to grab the CPU usage percentage

--- col_mon.sh
+++ col_mon.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #loop code infinite
-#top: grabs system data
-#-bn1: prints it as plain text
-cpu_stats=$(top -bn1)
 
+#Grabs the CPU idle usage percentage
+# top: grabs system data
+# -bn1: prints it as plain text
+cpu_stats=$(top -bn1 | grep "Cpu(s)" | awk -F'id,' '{print $1}' | awk '{print $NF}')
 echo "$cpu_stats"
 #get cpu usage
 
