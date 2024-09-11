34decf7cd611404552d498ff651d27d1797b53e9
Darajaw <darajaw@gmail.com>
2024-09-11 11:42:07 -0400

    Added top command to monitor system stats

--- col_mon.sh
+++ col_mon.sh
@@ -1,1 +1,12 @@
-test
+#!/bin/bash
+
+#loop code infinite
+#top: grabs system data
+#-bn1: prints it as plain text
+cpu_stats=$(top -bn1)
+
+echo "$cpu_stats"
+#get cpu usage
+
+#change terminal color based on cpu usage
+
